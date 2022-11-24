78f2625f92965d4d8d3fb0cd8e8a22f89cbec739
Никита <samnikita141204@yandex.ru>
2022-11-24 17:12:58 +0400

    первая и вторая задача урока 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
 
 //Console.WriteLine(" Введите трехзначное число") ;
 //int number = Convert.ToInt32(Console.ReadLine()) ;
@@ -6,11 +10,18 @@
 //sl1=sl1*10+number%10 ;
 //Console.WriteLine(sl1) ;
 
-//if (number1==(int)Math.Pow(number2, 2)) 
-  //{
-   //Console.WriteLine(" да ") ;
-  //} 
-   //else
-   //{
-    //Console.WriteLine(" нет ") ;
-   //}
+
+// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому.
+// Если второе число не кратно числу первому, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4 16, 4 -> кратно
+
+Console.Write(" Введите первое число ") ;
+int num1 = Convert.ToInt32(Console.ReadLine()) ;
+Console.Write(" Введите второе число ") ;
+int num2 = Convert.ToInt32(Console.ReadLine()) ;
+int ostat = num2%num1 ;
+if(ostat == 0)
+  Console.WriteLine(" число два кратно числу один ") ;
+else 
+Console.WriteLine($" числа два не кратно числу один, остаток {ostat}") ;
+
