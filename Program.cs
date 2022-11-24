3ab9f63f42ae489b3fecadc6f95ad267d601f95a
Никита <samnikita141204@yandex.ru>
2022-11-24 16:25:07 +0400

    Первое сохраненние

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Console.WriteLine(" Введите трехзначное число") ;
+//int number = Convert.ToInt32(Console.ReadLine()) ;
+//int sl1=number/100 ;
+//sl1=sl1*10+number%10 ;
+//Console.WriteLine(sl1) ;
+
+//if (number1==(int)Math.Pow(number2, 2)) 
+  //{
+   //Console.WriteLine(" да ") ;
+  //} 
+   //else
+   //{
+    //Console.WriteLine(" нет ") ;
+   //}
