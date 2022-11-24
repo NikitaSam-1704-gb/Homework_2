f8bb9bbea031f1241fc62445973b3d294266a491
Никита <samnikita141204@yandex.ru>
2022-11-24 17:23:47 +0400

    Третья задача урока 2

--- Program.cs
+++ Program.cs
@@ -15,13 +15,25 @@
 // Если второе число не кратно числу первому, то программа выводит остаток от деления.
 // 34, 5 -> не кратно, остаток 4 16, 4 -> кратно
 
-Console.Write(" Введите первое число ") ;
-int num1 = Convert.ToInt32(Console.ReadLine()) ;
-Console.Write(" Введите второе число ") ;
-int num2 = Convert.ToInt32(Console.ReadLine()) ;
-int ostat = num2%num1 ;
-if(ostat == 0)
-  Console.WriteLine(" число два кратно числу один ") ;
-else 
-Console.WriteLine($" числа два не кратно числу один, остаток {ostat}") ;
+//Console.Write(" Введите первое число ") ;
+//int num1 = Convert.ToInt32(Console.ReadLine()) ;
+//Console.Write(" Введите второе число ") ;
+//int num2 = Convert.ToInt32(Console.ReadLine()) ;
+//int ostat = num2%num1 ;
+//if(ostat == 0)
+//  Console.WriteLine(" число два кратно числу один ") ;
+//else 
+//Console.WriteLine($" числа два не кратно числу один, остаток {ostat}") ;
 
+
+//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+//14 -> нет 
+//46 -> нет 
+//161 -> да
+
+Console.Write(" Введите число ") ;
+int num = Convert.ToInt32(Console.ReadLine()) ;
+if(num%7 == 0 && num%23 == 0)
+  Console.WriteLine($" число  {num} КРАТНО 7 и 23 ") ;
+else 
+  Console.WriteLine($" число  {num}  НЕ КРАТНО 7 и 23") ;
