ce98aad862cf314c150cbb8afbc58c3e932c75d6
Никита <samnikita141204@yandex.ru>
2022-11-25 00:24:51 +0400

    Задача 13 Д/З

--- Program.cs
+++ Program.cs
@@ -117,12 +117,35 @@
 
 
 //Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-
 //456 -> 5
 //782 -> 8
 //918 -> 1
 
-Console.Write(" Введите трехзначное число  ") ;
+//Console.Write(" Введите трехзначное число  ") ;
+//int number = Convert.ToInt32(Console.ReadLine()) ;
+//int sl1=(number/10)%10 ;
+//Console.WriteLine($" вторая цифра числа {number} -> {sl1} ") ;
+
+
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.Write(" Введите число  ") ;
 int number = Convert.ToInt32(Console.ReadLine()) ;
-int sl1=(number/10)%10 ;
-Console.WriteLine($" вторая цифра числа {number} -> {sl1} ") ;
+int count = 0 ;
+int ostNumber=number ;
+while (ostNumber>1 && count<=3)
+{
+    ostNumber=ostNumber/10 ;
+    count++;
+}
+if(count>=3)
+{
+    number=(number/100)%10 ; 
+    Console.WriteLine($" значение третьего разряда -> {number} ") ;
+}    
+else 
+    Console.WriteLine($" Данное число имеет менее трех разрядов");
